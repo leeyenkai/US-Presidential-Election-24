aacbf7185067a1aa8eb41896e1402df4899e89f4
Lee Yen Kai <82197952+leeyenkai@users.noreply.github.com>
2023-10-10 20:31:31 +0800

    Add files via upload

---
+++ Data Cleaning [IN PROGRESS]/Data_Cleaning(Pres).R
@@ -0,0 +1,32 @@
+library("rcompanion") #this package is required for transformTukey function
+library("rstatix") # for Welch ANOVA test
+library("Rmisc")  # ci function to compute confidence interval
+library("rpivotTable")
+library("tidyverse")
+library("psych")
+library("RColorBrewer")
+library("corrplot")
+
+#Load main historical dataset 
+main_hist <- read.csv("president_polls_historical.csv")
+head(main_hist)
+str(main_hist)
+summary(main_hist)
+
+#Averages dataset
+main_avgs <- read.csv("presidential_general_averages.csv")
+head(main_avgs)
+str(main_avgs)
+summary(main_avgs)
+
+#Primary Averages dataset
+pri_avgs <- read.csv("presidential_primary_averages.csv")
+head(pri_avgs)
+str(pri_avgs)
+summary(pri_avgs)
+
+#Generic ballot polls dataset
+generic_hist <- read.csv("generic_ballot_polls.csv")
+head(generic_hist)
+str(generic_hist)
+summary(generic_hist)
